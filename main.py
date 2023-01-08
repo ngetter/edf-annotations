8fdb7492b40649b2a3d63adad654a863d1910161
Nir@vostro <getter@post.bgu.ac.il>
2023-01-08 19:57:40 +0200

    Title: Summary, imperative, start upper case, don't end with a period

--- main.py
+++ main.py
@@ -6,8 +6,13 @@ import mne
 import mne.annotations
 import pathlib
 
-SOURCE = r'D:\Documents\EEG Studies\isa.edf'
-DEST = r'D:\Documents\EEG Studies\isa.xlsx'
+from datetime import timedelta
+
+
+from PyQt5.QtWidgets import QFileDialog, QWidget, QApplication
+
+SOURCE = r'E:\assuta\data\SEEG'
+DEST = r'E:\assuta\data\SEEG'
 
 def _get_annotations(source, dest: str = ''):
     """
@@ -26,7 +31,7 @@ def _get_annotations(source, dest: str = ''):
     annotations = r.annotations
 
     annotations_df = annotations.to_data_frame()
-    return annotations_df
+    return annotations_df, r
 
 
 def _save_annotations_to_file(annotations_df: pd.DataFrame ,dest: pathlib.Path, patient_id: str):
@@ -39,17 +44,48 @@ def _save_annotations_to_file(annotations_df: pd.DataFrame ,dest: pathlib.Path, 
     Returns:
 
     """
-    with pd.ExcelWriter(dest) as excelf:
-        annotations_df.to_excel(excelf, sheet=patient_id)
+    with pd.ExcelWriter(dest / f'{patient_id}.xlsx', mode='a') as excelf:
+        annotations_df.to_excel(excelf)
+
+
+def get_times(start_time, num_samples, sampling_rate, file_path):
+    # Make the start time timezone unaware
+    start_time = start_time.replace(tzinfo=None)
+
+    # Calculate the duration of the samples in seconds
+    duration = num_samples / sampling_rate
+
+    # Calculate the end time by adding the duration to the start time
+    end_time = start_time + timedelta(seconds=duration)
+
+    # Make the end time timezone unaware
+    end_time = end_time.replace(tzinfo=None)
+
+    # Create a Pandas Series with the start and end times
+    times = pd.Series([start_time, end_time], index=['start_time', 'end_time'])
+
+    # Get the folder name from the file path
+    folder_name = file_path.parent.name
+
+    # Write the Pandas Series to an Excel file
+    writer = pd.ExcelWriter(file_path, engine='openpyxl')
+    times.to_excel(writer, sheet_name=folder_name)
+    writer.save()
+
 
 def _read_all_edf_in_root_folder(root: pathlib.Path):
     return root.glob('**/*.edf')
 
 def main(argv):
-    glob_edf = _read_all_edf_in_root_folder(pathlib.Path(SOURCE))
+    app = QApplication(argv)
+    window = QWidget()
+    # window.show()
+    dirname = str(QFileDialog.getExistingDirectory(window, "Select edf dir", SOURCE))
+    glob_edf = _read_all_edf_in_root_folder(pathlib.Path(dirname))
     for edf_file in glob_edf:
-        annotations_df = _get_annotations(edf_file.__str__())
-        _save_annotations_to_file(annotations_df, pathlib.Path(DEST), 'patient1')
+        annotations_df, obj = _get_annotations(edf_file.__str__())
+        get_times(obj.info['meas_date'], obj.n_times, obj.info['sfreq'], pathlib.Path(dirname) / 'timings.xlsx')
+        _save_annotations_to_file(annotations_df, pathlib.Path(dirname), 'patient1')
 
 if __name__=='__main__':
     main(sys.argv)
